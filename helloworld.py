3f695854e1ecd8e3cf5ced555b15b3d6c5171c1f
Kasonde Besa <61287639+KayBesa@users.noreply.github.com>
2020-09-27 21:42:55 -0400

    First Commit

---
+++ helloworld.py
@@ -0,0 +1,11 @@
+# Kasonde Besa
+# helloworld.py
+# Basically the first program every programmer has to develop.
+# Say hello to the programming world! But, with a bit of a twist.
+
+def main():
+  Greeting = "Hello, world of Python3!"
+  print("{}",format(Greeting))
+
+if __name__ == "__main__":
+    main()
