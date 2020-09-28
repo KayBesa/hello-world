94c668fa19088a5b9501d37edafbb15253905909
Kasonde Besa <61287639+KayBesa@users.noreply.github.com>
2020-09-27 21:51:21 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/* Kasonde Besa
+ * helloworld.java
+ * September 27th, 2020
+ * Basic hello world program in java.
+ */
+ 
+ public Hello{
+    public static void main (String [] args){
+        System.out.println("Hello World of Java!");
+    }
+ }
