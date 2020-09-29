6b1f9d65c984768fab8289af7c84bc7784e9aa68
Kasonde Besa <61287639+KayBesa@users.noreply.github.com>
2020-09-28 21:15:50 -0400

    Create helloworld.go

---
+++ helloworld.go
@@ -0,0 +1,12 @@
+// Kasonde Besa
+// helloworld.go
+// Simple hello world program in Go.
+// September 28th, 2020.
+
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("Hello, World of Go!")
+}
