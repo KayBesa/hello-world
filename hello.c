46068541bd8725f68c8df78c4b578f1479726f1f
Kasonde Besa <61287639+KayBesa@users.noreply.github.com>
2020-09-28 21:04:18 -0400

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+/* Kasonde Besa
+ * hello.c
+ * Simple hello world program in C
+ * September 28th, 2020.
+ */
+
+#include <stdio.h>
+
+int main(){
+    printf("%s\n","Hello, World of C!");
+    return 0;
+}
